09279fbaf5177d4a151268f5ac2dfa1a62e9dbec
avgays <avgays@gmail.com>
2019-01-07 13:14:20 +0300

    Add Filter Service

--- index.js
+++ index.js
@@ -14,7 +14,8 @@
       "humID":359,
       "sensorID":386,
       "waterID":360,
-      "tagHum":2
+      "tagHum":2,
+      "filterLevel":3
   }]
 }
 */
@@ -29,12 +30,13 @@ module.exports = function (homebridge) {
 
 function myHumidifier(log, config) {
   this.log = log;
-  this.name = config["name"];
-  this.domoUrl = config['domoUrl'];
-  this.humID = config['humID'];
-  this.sensorID = config['sensorID']
-  this.waterID = config['waterID'];
-  this.tagHum = config['tagHum'];
+  this.name = config.name;
+  this.domoUrl = config.domoUrl;
+  this.humID = config.humID;
+  this.sensorID = config.sensorID;
+  this.waterID = config.waterID;
+  this.tagHum = config.tagHum;
+  this.filterLevel = (config.filterLevel || 3);
 }
 
 myHumidifier.prototype = {
@@ -46,11 +48,9 @@ myHumidifier.prototype = {
         .setCharacteristic(Characteristic.SerialNumber, this.humID + "-" + this.sensorID + "-" + this.waterID);
    
       let HumidifierService = new Service.HumidifierDehumidifier(this.name);
-
       HumidifierService
         .getCharacteristic(Characteristic.CurrentRelativeHumidity)
         .on('get', this.getHumidity.bind(this));
-        
       HumidifierService
         .getCharacteristic(Characteristic.RotationSpeed)
         .on('get', this.getRotationSpeed.bind(this))
@@ -60,29 +60,40 @@ myHumidifier.prototype = {
           minValue: 0,
           minStep: 33
         });
-        
       HumidifierService
         .getCharacteristic(Characteristic.RelativeHumidityHumidifierThreshold)
         .on('get', this.getTargetHumidy.bind(this))
         .on('set', this.setTargetHumidy.bind(this));
-
       HumidifierService
         .getCharacteristic(Characteristic.WaterLevel)
         .on('get', this.getWaterLevel.bind(this));
-
       HumidifierService
         .getCharacteristic(Characteristic.Active)
         .on('get', this.getActive.bind(this))
         .on('set', this.setActive.bind(this));
-
       HumidifierService
         .setCharacteristic(Characteristic.CurrentHumidifierDehumidifierState,Characteristic.CurrentHumidifierDehumidifierState.HUMIDIFYING)
         .setCharacteristic(Characteristic.TargetHumidifierDehumidifierState, Characteristic.TargetHumidifierDehumidifierState.HUMIDIFIER)
         .setCharacteristic(Characteristic.LockPhysicalControls, Characteristic.LockPhysicalControls.CONTROL_LOCK_ENABLED);
 
+///////////////////
+      let FilterService = new Service.FilterMaintenance(this.name);        
+      FilterService
+        .getCharacteristic(Characteristic.FilterLifeLevel)
+        .on('get', this.getFilterLifeLevel.bind(this));
+      FilterService
+        .getCharacteristic(Characteristic.FilterChangeIndication)
+        .on('get', this.getFilterChange.bind(this));
+      FilterService
+        .getCharacteristic(Characteristic.ResetFilterIndication)
+        .on('get', this.setResetFilter.bind(this))
+        .on('set', this.ResetFilter.bind(this));
+///////////////////
+        
       this.informationService = informationService;
       this.HumidifierService = HumidifierService;
-      return [informationService, HumidifierService];
+      this.FilterService = FilterService;
+      return [informationService, HumidifierService,FilterService];
     }
 };
 
@@ -104,7 +115,7 @@ myHumidifier.prototype.getHumidity = function (callback) {
       var humidity = json.result[0].Humidity;
       //me.log("humidity " + humidity );
       return callback(null, humidity); 
-    })
+    });
 };
 
 //////////////////////RotationSpeed
@@ -124,7 +135,7 @@ myHumidifier.prototype.getRotationSpeed = function (callback) {
     var RotationSpeed = json.result[0].LevelInt;
     //me.log("RotationSpeed " + RotationSpeed );
     return callback(null, RotationSpeed); 
-  })
+  });
 };
 
 myHumidifier.prototype.setRotationSpeed = function (RotationSpeed, callback) {
@@ -133,17 +144,17 @@ myHumidifier.prototype.setRotationSpeed = function (RotationSpeed, callback) {
       url: me.domoUrl + "json.htm?type=command&param=switchlight&idx=" + me.humID + "&switchcmd=Set%20Level&level=" + RotationSpeed, 
       method: 'GET',
   },           
-  function (error, response, body) {
+  function (error, response) {
     if (error) {
       me.log('STATUS: ' + response.statusCode);
       me.log(error.message);
       return callback(error);
     }
-    var json = JSON.parse(body);
-    var status = json.status;
+    //var json = JSON.parse(body);
+    //var status = json.status;
     //me.log("Set RotationSpeed %s : %s", RotationSpeed, status );
     return callback(null); 
-  })
+  });
 };
 
 //////////////////////Active
@@ -164,7 +175,7 @@ myHumidifier.prototype.getActive = function (callback) {
     
     //me.log("Active " + Active );
     return callback(null, Active); 
-  })
+  });
 };
 myHumidifier.prototype.setActive = function (Active, callback) {
   const me = this;
@@ -173,17 +184,17 @@ myHumidifier.prototype.setActive = function (Active, callback) {
       url: me.domoUrl + "json.htm?type=command&param=switchlight&idx=" + me.humID + "&switchcmd=" + Active, 
       method: 'GET',
   },           
-  function (error, response, body) {
+  function (error, response) {
     if (error) {
       me.log('STATUS: ' + response.statusCode);
       me.log(error.message);
       return callback(error);
     }
-    var json = JSON.parse(body);
-    var status = json.status;
+    //var json = JSON.parse(body);
+    //var status = json.status;
     //me.log("Set Active %s : %s", Active, status );
     return callback(null); 
-  })
+  });
 };
 
 //////////////////////getWaterLevel
@@ -203,7 +214,7 @@ myHumidifier.prototype.getWaterLevel = function (callback) {
     var WaterLevel = parseInt(json.result[0].Data);
     //me.log("WaterLevel " + WaterLevel );
     return callback(null, WaterLevel); 
-  })
+  });
 };
 
 /////////////////////TargetHumidy
@@ -223,13 +234,33 @@ myHumidifier.prototype.getTargetHumidy = function (callback) {
     var TargetHumidy = json.result[0].Value;
     //me.log("TargetHumidy " + TargetHumidy );
     return callback(null, TargetHumidy); 
-  })
+  });
 };
 
 myHumidifier.prototype.setTargetHumidy = function (TargetHumidy, callback) {
   const me = this;
   request.get({
       url: me.domoUrl + "json.htm?type=command&param=updateuservariable&vname=TargetHumidy&vtype=0&vvalue=" + TargetHumidy, 
+      method: 'GET',
+  },           
+  function (error, response) {
+    if (error) {
+      me.log('STATUS: ' + response.statusCode);
+      me.log(error.message);
+      return callback(error);
+    }
+    //var json = JSON.parse(body);
+    //var status = json.status;
+    //me.log("Set TargetHumidy %s : %s", TargetHumidy, status );
+    return callback(null); 
+  });
+};
+
+
+myHumidifier.prototype.getFilterLifeLevel = function (callback) {
+  const me = this;
+  request.get({
+      url: me.domoUrl + "json.htm?type=command&param=getuservariable&idx=" + me.filterLevel, 
       method: 'GET',
   },           
   function (error, response, body) {
@@ -239,8 +270,43 @@ myHumidifier.prototype.setTargetHumidy = function (TargetHumidy, callback) {
       return callback(error);
     }
     var json = JSON.parse(body);
-    var status = json.status;
-    //me.log("Set TargetHumidy %s : %s", TargetHumidy, status );
+    var FilterLifeLevel = parseInt(json.result[0].Value);
+    return callback(null, FilterLifeLevel); 
+  });  
+};
+
+myHumidifier.prototype.getFilterChange = function (callback) {
+  const me = this;
+  request.get({
+      url: me.domoUrl + "json.htm?type=command&param=getuservariable&idx=" + me.filterLevel, 
+      method: 'GET',
+  },           
+  function (error, response, body) {
+    if (error) {
+      me.log('STATUS: ' + response.statusCode);
+      me.log(error.message);
+      return callback(error);
+    }
+    var json = JSON.parse(body);
+    var FilterChange = (parseInt(json.result[0].Value)>5)?0:1;
+    return callback(null, FilterChange); 
+  }); 
+};
+myHumidifier.prototype.ResetFilter = function (callback) {
+  const me = this;
+  request.get({
+      url: me.domoUrl + "json.htm?type=command&param=updateuservariable&vname=HumidifierLiquid&vtype=1&vvalue=100", 
+      method: 'GET',
+  },
+  function (error, response) {
+    if (error) {
+      me.log('STATUS: ' + response.statusCode);
+      me.log(error.message);
+      return callback(error);
+    }
     return callback(null); 
-  })
+  });
+};
+myHumidifier.prototype.setResetFilter = function (callback) {
+  return callback(null, 1); 
 };
